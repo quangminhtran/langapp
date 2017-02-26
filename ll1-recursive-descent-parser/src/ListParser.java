766ce258ecde758242e99bd51e592a452c649999
Quang Minh Tran <minhquang104@gmx.de>
2017-02-26 21:20:40 +0100

    Restore LL1 recursive-descent parser for nested list

---
+++ ll1-recursive-descent-parser/src/ListParser.java
@@ -0,0 +1,36 @@
+public class ListParser extends Parser {
+
+	public ListParser(Lexer lexer) {
+		super(lexer);
+	}
+	
+	@Override
+	public Token match(int type) {
+		return super.match(type);
+	}
+
+	/** '[' elements ']' */
+	public void list() {
+		match(ListLexer.LBRACK);
+		elements();
+		match(ListLexer.RBRACK);
+	}
+
+	/** element (',' element)* */
+	private void elements() {
+		element();
+		while (lookahead.type == ListLexer.COMMA) {
+			match(ListLexer.COMMA);
+			element();
+		}
+	}
+	
+	/** element: NAME | list */
+	private void element() {
+		if (lookahead.type == ListLexer.NAME) {
+			match(ListLexer.NAME);
+		} else if (lookahead.type == ListLexer.LBRACK) {
+			list();
+		} else throw new Error("Expect either name or list " + " but  got " + lookahead);
+	}
+}
